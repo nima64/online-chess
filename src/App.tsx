d7cafb9e24c076e8cad3118fc95362a0c6997ee6
nima64 <kennethe407@gmail.com>
2022-01-06 07:48:31 -0800

    display checkerbaord

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
+
+
+function Square(props: any) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function Game() {
+  const boardDim = 8; // number of tiles 
+  const boardSize = 300; // hieght and width of board
+
+  let grid: number[] = Array(boardDim*boardDim).fill(null);
+  const getBoardColor = (i:number):number => (i+Math.floor(i/boardDim)+1)%2;
+
+  return (
+    <div id="Game" style={{ display: "grid", height:`${boardSize}px`, width:`${boardSize}px`, gridTemplateColumns: `repeat(${boardDim},1fr)`}}>
+      {
+        grid.map((v, i) => (<div style={{backgroundColor:`${getBoardColor(i) == 0?'blue':'white'}`}}/>))
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 }
